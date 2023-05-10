0ffb2737d8eb726da8aa78807a307a9418ac6f4c
amine-kaabi <aminekabbi500@gmail.com>
2023-05-10 05:31:46 +0100

    integration de xo ajouté

--- integrationn/main.c
+++ integrationn/main.c
@@ -12,6 +12,7 @@
 #include <SDL/SDL_ttf.h>
 #include "time.h"
 #include "map.h"
+#include "entete.h"
 #include <string.h>
 #include"collisionparfaite.h"
 int main(int argc, char *args[])
@@ -19,6 +20,9 @@ int main(int argc, char *args[])
     /////////////////////////////
     int continuer1=1,done1=1;
     int x2=0;
+    tic tt;
+    int coup=1;
+    int a,b;	
     SDL_Event event;
     int done=1;
     int continuer = 3;
@@ -48,7 +52,8 @@ int main(int argc, char *args[])
     /////////////////////////////
     initBackMain(&Main);
     initText(&t);
-enig = IMG_Load("1.png");
+	initialisation(&tt);
+	enig = IMG_Load("1.png");
 	enigpos.x=100;
 	enigpos.y=100;
 	
@@ -72,8 +77,6 @@ enig = IMG_Load("1.png");
     SDL_EnableKeyRepeat(0, 0);//bich ya9rali min clavier taktak
     while (continuer)
     {
-       // for (i = 0; i < 10 && continuer; i++)
-        //{
             for (j = 0; j < 2; j++)
             {
                 AfficherMain(Main, screen, j);
@@ -91,200 +94,274 @@ enig = IMG_Load("1.png");
                 case SDL_QUIT:
                     continuer = 0;
                     break;
-
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym)
                     {
                     case SDLK_ESCAPE:
                         continuer = 0;
                         break;
-
                     case SDLK_j:
                         while (continuer1)
-	{	
-	SDL_PollEvent(&event);
-	switch(event.type)
-        {
-
-        case SDL_QUIT:
-            continuer=0;
-            break;
-        case SDL_KEYUP:
-            switch(event.key.keysym.sym)
-            {
-            case SDLK_k:{
-		while(done){
-		update_timeenig(&tempsenig);
-		SDL_BlitSurface(enig,NULL,screen,&enigpos);
-		displaytime(tempsenig,screen);
-		SDL_Flip(screen);
-		if(tempsenig.ss==temps.ss+7)
-		{
-                  done=0;
-		}
-		}}
-                break ;
-
-	
-	    
-	}}	
-		
-                collision(backgm,&p);
-		update_time(&temps);
-		deplacer_perso(&p,event);
-                 incrimenter(&x2);
-		perso_map(&m,&p,screen,x2);
-		SDL_BlitSurface(backg,NULL,screen,&backg_pos);
-		
-		displaytime(temps,screen);
-		
-		afficher(m,screen);
-		afficher_perso(screen,p);
-		SDL_Flip(screen);
-                        break;
-
+	      		{	
+				SDL_PollEvent(&event);
+				switch(event.type)
+        			{
+        				case SDL_QUIT:
+            					continuer1=0;
+            					break;
+        				case SDL_KEYUP:
+            					switch(event.key.keysym.sym)
+            					{
+            						case SDLK_k:
+	    						{
+								while(done)
+								{
+									affichage(tt,screen); 
+									SDL_Flip(screen);
+									if( tt.tour<9 && atilganer(tt.tabsuivi)==0)
+									{
+										if( (tt.tour)%2==0) 
+										{
+											calcul_coup(tt.tabsuivi);
+											tt.tour++;
+            									}
+        									else
+										{
+											SDL_WaitEvent(&event);
+											switch(event.type)
+											{
+												case SDL_QUIT:
+			    										continuer=0;
+			    										break;
+												case SDL_MOUSEBUTTONUP:
+		 											if (event.button.button == SDL_BUTTON_LEFT)
+                											{
+								    						a=event.button.x/184;
+								    						b=event.button.y/180;
+								    						coup=3*b+a;
+								    						tt.tour++;
+							    							tt.tabsuivi[coup]=-1;
+          												}
+            												break;
+        										}
+										}
+									}
+									else
+									{ 
+ 										Resultat(tt,screen);
+ 										continuer=0;   
+									}
+								}
+	    						}
+            						break;
+						}
+				}	
+                		collision(backgm,&p);
+				update_time(&temps);
+				deplacer_perso(&p,event);
+                		incrimenter(&x2);
+				perso_map(&m,&p,screen,x2);
+				SDL_BlitSurface(backg,NULL,screen,&backg_pos);
+				displaytime(temps,screen);
+				afficher(m,screen);
+				afficher_perso(screen,p);
+				SDL_Flip(screen);
+                        	break;
                     case SDLK_s:
                         Mix_PlayChannel(-1, M.bref, 0);
                         Settings(screen, &Mode, &niv_volume, &volume);
                         break;
 		    case SDLK_k:
                     {
-		while(done){
-		update_timeenig(&tempsenig);
-		SDL_BlitSurface(enig,NULL,screen,&enigpos);
-		displaytime(tempsenig,screen);
-		SDL_Flip(screen);
-		if(tempsenig.ss==temps.ss+7)
-		{
-                  done=0;
-		}
-		}}
-                break ;
+			while(done)	
+			{
+									affichage(tt,screen); 
+									SDL_Flip(screen);
+									if( tt.tour<9 && atilganer(tt.tabsuivi)==0)
+									{
+										if( (tt.tour)%2==0) 
+										{
+											calcul_coup(tt.tabsuivi);
+											tt.tour++;
+            									}
+        									else
+										{
+											SDL_WaitEvent(&event);
+											switch(event.type)
+											{
+												case SDL_QUIT:
+			    										continuer=0;
+			    										break;
+												case SDL_MOUSEBUTTONUP:
+		 											if (event.button.button == SDL_BUTTON_LEFT)
+                											{
+								    						a=event.button.x/184;
+								    						b=event.button.y/180;
+								    						coup=3*b+a;
+								    						tt.tour++;
+							    							tt.tabsuivi[coup]=-1;
+          												}
+            												break;
+        										}
+										}
+									}
+									else
+									{ 
+ 										Resultat(tt,screen);
+ 										continuer=0;   
+									}
+			}
+		    }
+                    break ;
                     case SDLK_UP:
                         Presses = Presses % 3 - 1;
-                        if (Presses == 0)
-                            Presses = 3;
-                        if (Presses == -1)
-                            Presses = 2;
+                        if (Presses == 0) Presses = 3;
+                        if (Presses == -1) Presses = 2;
                         SDL_Delay(100);
                         break;
-
                     case SDLK_DOWN:
                         Presses = Presses % 3 + 1;
                         SDL_Delay(100);
                         break;
-
                     case SDLK_SPACE:
                         switch (Presses)
                         {
-                        case 1:
-                             while (continuer1)
-	{	
-	SDL_PollEvent(&event);
-	switch(event.type)
-        {
-
-        case SDL_QUIT:
-            continuer=0;
-            break;
-        case SDL_KEYUP:
-            switch(event.key.keysym.sym)
-            {
-            case SDLK_k:{
-		while(done){
-		update_timeenig(&tempsenig);
-		SDL_BlitSurface(enig,NULL,screen,&enigpos);
-		displaytime(tempsenig,screen);
-		SDL_Flip(screen);
-		if(tempsenig.ss==temps.ss+7)
-		{
-                  done1=0;
-		}
-		}}
-                break ;
-
-	
-	    
-	}}	
-		
-                collision(backgm,&p);
-		update_time(&temps);
-		deplacer_perso(&p,event);
-                 incrimenter(&x2);
-		perso_map(&m,&p,screen,x2);
-		SDL_BlitSurface(backg,NULL,screen,&backg_pos);
-		
-		displaytime(temps,screen);
-		
-		afficher(m,screen);
-		afficher_perso(screen,p);
-		SDL_Flip(screen);
-
-	}
-
-                            break;
-                        case 2:
-                            Settings(screen, &Mode, &niv_volume, &volume);
-                            break;
-                        case 3:
-                            continuer = 0;
-                            break;
+                        	case 1:
+                             		while (continuer1)
+					{	
+						SDL_PollEvent(&event);
+						switch(event.type)
+        					{
+        					case SDL_QUIT:
+           						continuer1=0;
+            						break;
+        					case SDL_KEYUP:
+            						switch(event.key.keysym.sym)
+            						{
+            							case SDLK_k:
+								{
+									while(done)	
+									{
+										affichage(tt,screen); 
+									SDL_Flip(screen);
+									if( tt.tour<9 && atilganer(tt.tabsuivi)==0)
+									{
+										if( (tt.tour)%2==0) 
+										{
+											calcul_coup(tt.tabsuivi);
+											tt.tour++;
+            									}
+        									else
+										{
+											SDL_WaitEvent(&event);
+											switch(event.type)
+											{
+												case SDL_QUIT:
+			    										continuer=0;
+			    										break;
+												case SDL_MOUSEBUTTONUP:
+		 											if (event.button.button == SDL_BUTTON_LEFT)
+                											{
+								    						a=event.button.x/184;
+								    						b=event.button.y/180;
+								    						coup=3*b+a;
+								    						tt.tour++;
+							    							tt.tabsuivi[coup]=-1;
+          												}
+            												break;
+        										}
+										}
+									}
+									else
+									{ 
+ 										Resultat(tt,screen);
+										done=0;
+ 										continuer=0;   
+									}
+										SDL_PollEvent(&event);
+										switch(event.type)
+										{
+											case SDL_KEYDOWN:
+												switch(event.key.keysym.sym)
+												{
+													case SDLK_ESCAPE:
+														done=0;
+														break;
+            											}
+										}
+									}
+                						}
+								break;	
+							}
+						}	
+                				collision(backgm,&p);
+					        update_time(&temps);
+						deplacer_perso(&p,event);
+                 				incrimenter(&x2);
+						perso_map(&m,&p,screen,x2);
+						SDL_BlitSurface(backg,NULL,screen,&backg_pos);
+						displaytime(temps,screen);
+						afficher(m,screen);
+						afficher_perso(screen,p);
+						SDL_Flip(screen);
+					}
+                            		break;
+				        case 2:
+				            	Settings(screen, &Mode, &niv_volume, &volume);
+				            	break;
+                        		case 3:
+                            			continuer = 0;
+                            			break;
                         }
-                        break;
                     }
                     break;
-
-                case SDL_MOUSEMOTION://win bich n7arek el souris
-                    if (event.motion.x > 430 && event.motion.x < 580 && event.motion.y > 250 && event.motion.y < 291)
-                    {
-                        hover1 = 0;
-                        Presses = 5;//5tr cest bon m3ach nst3ml clavier nfrechi
-                        Mix_FadeInChannel(-1, M.bref, 0, 0);
-                    }
-                    else if (event.motion.x > 430 && event.motion.x < 580 && event.motion.y > 350 && event.motion.y < 391)
-                    {
-                        hover2 = 0;
-                        Presses = 5;
-                        Mix_FadeInChannel(-1, M.bref, 0, 0);
+                    case SDL_MOUSEMOTION://win bich n7arek el souris
+                    		if (event.motion.x > 430 && event.motion.x < 580 && event.motion.y > 250 && event.motion.y < 291)
+                    		{
+				        hover1 = 0;
+				        Presses = 5;//5tr cest bon m3ach nst3ml clavier nfrechi
+				        Mix_FadeInChannel(-1, M.bref, 0, 0);
+				}
+                    		else if (event.motion.x > 430 && event.motion.x < 580 && event.motion.y > 350 && event.motion.y < 391)
+                    		{
+				        hover2 = 0;
+				        Presses = 5;
+				        Mix_FadeInChannel(-1, M.bref, 0, 0);
                     }
                     else if (event.motion.x > 430 && event.motion.x < 580 && event.motion.y > 450 && event.motion.y < 491)
                     {
-                        hover3 = 0;
-                        Presses = 5;
-                        Mix_FadeInChannel(-1, M.bref, 0, 0);
+                        	hover3 = 0;
+                        	Presses = 5;
+                        	Mix_FadeInChannel(-1, M.bref, 0, 0);
                     }
                     break;
-
-                case SDL_MOUSEBUTTONUP:
-                    if (event.button.button == SDL_BUTTON_LEFT)
-                    {
-                        if (event.button.x > 430 && event.button.x < 580 && event.button.y > 250 && event.button.y < 291)
-                        {
-                           collision(backgm,&p);
-		update_time(&temps);
-		deplacer_perso(&p,event);
-                 incrimenter(&x2);
-		perso_map(&m,&p,screen,x2);
-		SDL_BlitSurface(backg,NULL,screen,&backg_pos);
-		
-		displaytime(temps,screen);
-		
-		afficher(m,screen);
-		afficher_perso(screen,p);
-		SDL_Flip(screen);
-                        }
-                        else if (event.button.x > 430 && event.button.x < 580 && event.button.y > 350 && event.button.y < 391)
-                        {
-                            Settings(screen, &Mode, &niv_volume, &volume);
-                        }
-                        else if (event.button.x > 430 && event.button.x < 580 && event.button.y > 450 && event.button.y < 491)
-                        {
-                            continuer = 0;
-                            break;
-                        }
-                    }
-                    break;
-		
+		    case SDL_MOUSEBUTTONUP:
+                    	if (event.button.button == SDL_BUTTON_LEFT)
+                    	{
+                        	if (event.button.x > 430 && event.button.x < 580 && event.button.y > 250 && event.button.y < 291)
+                        	{
+                           		collision(backgm,&p);
+					update_time(&temps);
+					deplacer_perso(&p,event);
+					incrimenter(&x2);
+					perso_map(&m,&p,screen,x2);
+					SDL_BlitSurface(backg,NULL,screen,&backg_pos);
+					displaytime(temps,screen);
+					afficher(m,screen);
+					afficher_perso(screen,p);
+					SDL_Flip(screen);
+                        	}
+                        	else if (event.button.x > 430 && event.button.x < 580 && event.button.y > 350 && event.button.y < 391)
+                        	{
+                            		Settings(screen, &Mode, &niv_volume, &volume);
+                        	}
+                        	else if (event.button.x > 430 && event.button.x < 580 && event.button.y > 450 && event.button.y < 491)
+                        	{
+                            		continuer = 0;
+                            		break;
+                        	}
+                    	}
+                    	break;
                 }
             }
         }
